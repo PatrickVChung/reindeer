a4d1eb9f79c69cac2a6adeb91385a056548b55de
Patrick Chung <patrickvchung@gmail.com>
2026-02-24 10:15:26 -0800

    Fixed issues mentioned by Cam on email 2/20/2025. Details are in the email such filtering and sorting issues.

--- app/models/course_schedule.rb
+++ app/models/course_schedule.rb
@@ -1,6 +1,7 @@
 class CourseSchedule < ApplicationRecord
   validates :course_id, :year, :block, :start_date, :end_date, :no_of_seats, presence: true
   belongs_to :course, optional: false  # want the activerecord to enforce the association integrity
+  # scope :available, -> { where("no_of_seats IS NOT NULL AND no_of_seats != 0")
 
   COMP_CODES = ["ics1", "ics2", "ics3", "ics4", "ics5", "mk1", "mk2", "mk3", "pbli1", "pbli2", "pbli3", "pcp1", "pcp2", "pcp3", "pppd1", "pppd2",  "sbp1"]
 
