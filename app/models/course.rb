faf3eca48c01397be390fcc460b4c70ffb17d020
Patrick Chung <patrickvchung@gmail.com>
2026-03-31 09:10:39 -0700

    Course: Fixed two issues on the filters, discovered by Cam.  Fixed a minor issue in the Dashboard helper about object being nil.

--- app/models/course.rb
+++ app/models/course.rb
@@ -4,6 +4,24 @@ class Course < ApplicationRecord
   # scope :with_categories, ->(cats[]) { where(category: cats[]) if cats[].present }
   # scope :with_departments, ->(depts) { where(department: depts) if depts.present}
   # scope :with_durations, ->(weeks) { where(duration: weeks) if weeks.present }
+  # app/models/course.rb
+  scope :search, ->(term) {
+    where(
+      "course_number ILIKE :q OR course_name ILIKE :q OR course_purpose_statement ILIKE :q",
+      q: "%#{term}%"
+    )
+  }
+
+  scope :available_and_has_comment, -> {
+    joins(:course_schedules)
+      .where.not(course_schedules: { no_of_seats: [0, nil] })
+      .or(
+        joins(:course_schedules)
+          .where(course_schedules: { no_of_seats: [0, nil] })
+          .where.not(course_schedules: { comment: nil })
+      ).distinct
+  }
+
 
 # Add more scopes for filtering
 
