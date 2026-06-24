1f8169e264b523c1ed7ef416537375ac433b7a4a
Patrick Chung <patrickvchung@gmail.com>
2026-06-24 05:25:21 -0700

    FoM: Rewrote the send_alerts method. Courses:  Added attachments for Intersession courses.

--- app/mailers/fom_exam_mailer.rb
+++ app/mailers/fom_exam_mailer.rb
@@ -1,5 +1,5 @@
 class FomExamMailer < ApplicationMailer
-  def alert_student (to_user, from_user, subject, body)
+  def alert_student (from_user, to_user, subject, body)
       mail(
         to: to_user,
         from: from_user,
